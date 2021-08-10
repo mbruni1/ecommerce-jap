e6f832c9bff200cc814f11146bb9190933d35ce1
mbruni1 <manugus20@hotmail.com>
2021-08-10 19:11:34 +0200

    se agrego login que tenia en otro html y se modifico login js para linkear

--- js/login.js
+++ js/login.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
+    document.getElementById("botton").addEventListener("click", function(e) {  
+    window.location.href = "./index.html"
+    });
+     
+
+
+  
 });
+
