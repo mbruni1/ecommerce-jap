beef410dac433401b58afd00bb3cceabcbcad918
mbruni1 <manugus20@hotmail.com>
2021-08-12 21:38:43 -0300

    se cambio nombre de index a home y login a index para git pages inicie bien. se enlazo json en products. se agrego foto de back en index

--- js/login.js
+++ js/login.js
@@ -3,13 +3,30 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
-    document.getElementById("botton").addEventListener("click", function (e) {
-        window.location.href = "./index.html"
-    
+    document.getElementById("button").addEventListener("click", function (e) {
+        validarFormulario(e);
+
+        function validarFormulario(evento) {
+            evento.preventDefault();
+            var usuario = document.getElementById('usuario').value;
+            if (usuario.length == 0) {
+                alert('No has escrito nada en el usuario');
+                return;
+            }
+            var clave = document.getElementById('clave').value;
+            if (clave.length == 0) {
+                alert('La clave no es válida');
+                return;
+
+            }
+            else {
+                window.location.href = "./home.html";
+            }
+        }
+
+
+
+
     });
-     
-
-
-  
 });
 
